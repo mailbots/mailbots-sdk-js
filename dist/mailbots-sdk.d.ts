26206eb5db2f2a7d5246fd12609a5db91107b0c3
sebestindragos <sebestin.dragos@gmail.com>
2019-05-13 16:28:17 +0300

    Add types for Tasks. #3

--- dist/mailbots-sdk.d.ts
+++ dist/mailbots-sdk.d.ts
@@ -1,17 +1,16 @@
 import {AxiosRequestConfig} from "axios";
 
 export interface Task {
-  command: string
-}
-
-export interface Email {
   command: string,
-  to: string,
-  cc: string[],
-  bcc: string[],
-  from: string,
-  subject: string,
-  body: string | Array<{type: string, text: string}>
+  trigger_timeformat: string | null,
+  completed: boolean,
+  reference_email: {
+    to: string,
+    cc: string,
+    bcc: string,
+    subject: string,
+    html: string
+  }
 }
 
 export default class MailBotsClient {
@@ -84,37 +83,43 @@ export default class MailBotsClient {
    *
    * @example
    * const res = await mbClient.createTask(
-    *     {
-    *       webhook: false,
-    *       suppress_email: true,
-    *       verbose: 1,
-    *       task: {
-    *         command: process.env.EXAMPLE_COMMAND
-    *       },
-    *       send_messages: [
-    *         {
-    *           type: "email",
-    *           subject: "A test email message",
-    *           to: "test@example.com",
-    *           body: [
-    *             {
-    *               type: "html",
-    *               text: "<h1>This is a test</h1>"
-    *             }
-    *           ]
-    *         }
-    *       ]
-    *     });
-    *
-    */
+   *     {
+   *       webhook: false,
+   *       suppress_email: true,
+   *       verbose: 1,
+   *       task: {
+   *         command: process.env.EXAMPLE_COMMAND
+   *       },
+   *       send_messages: [
+   *         {
+   *           type: "email",
+   *           subject: "A test email message",
+   *           to: "test@example.com",
+   *           body: [
+   *             {
+   *               type: "html",
+   *               text: "<h1>This is a test</h1>"
+   *             }
+   *           ]
+   *         }
+   *       ]
+   *     });
+   *
+   */
    createTask(params: {
     webhook: boolean,
     verbose: number,
     suppress_email: boolean,
-    task: Task
+    task: Task,
+    send_messages: Array<{
+      type: string,
+      subject: string,
+      to: string,
+      body: Array<{type: string, text: string}>
+    }>
    }, cb: Function): Promise<any>;
 
-   /**
+  /**
    * Send email and automatically create a Task
    * This creates a MailBots task and sends an email. It's a wrapper
    * for createTask with opinionated settings for just sending email.
@@ -123,19 +128,163 @@ export default class MailBotsClient {
    * @return {Promise}
    * @example
    *  const res = await mbClient.sendEmail({
-    *     command: "command@my-ext.eml.bot", // must be your bot's domain
-    *     to: "test@exampletask.com",
-    *     cc: [],
-    *     bcc: [],
-    *     from: "test@example.com",
-    *     subject: "Test1",
-    *     body: [
-    *       {
-    *         type: "html",
-    *         text: "<h1>This is a test</h1>"
-    *       }
-    *     ]
-    *   });
+   *     command: "command@my-ext.eml.bot", // must be your bot's domain
+   *     to: "test@exampletask.com",
+   *     cc: [],
+   *     bcc: [],
+   *     from: "test@example.com",
+   *     subject: "Test1",
+   *     body: [
+   *       {
+   *         type: "html",
+   *         text: "<h1>This is a test</h1>"
+   *       }
+   *     ]
+   *   });
+   */
+  sendEmail(email: {
+    command: string,
+    to: string,
+    cc: string[],
+    bcc: string[],
+    from: string,
+    subject: string,
+    body: string | Array<{type: string, text: string}>,
+    trigger_timeformat?: string
+  }, cb: Function): Promise<any>;
+
+  /**
+   * Send emails relating to an existing Task. Does not mutate task.
+   * NOTE: This may be deprecated in favor of sending messages via the update task endpoint.
+   * @param {object} params
+   * @param {number} params.task.id
+   * @param {Array} params.messages
+   *
+   * @example
+   *  const res = await mbClient.sendMessages({
+   *   task: {
+   *     id: 123
+   *   },
+   *   send_messages: [{
+   *     to: "test@exampletask.com",
+   *     cc: [],
+   *     bcc: [],
+   *     from: "test@example.com",
+   *     subject: "Test1",
+   *     body: [
+   *       {
+   *         type: "html",
+   *         text: "<h1>This is a test</h1>"
+   *       }
+   *     ]
+   *   }]
+   *  });
+   */
+  sendMessages(params: {
+    task: {
+      id: number
+    },
+    send_messages: Array<{
+      to: string,
+      cc: string[],
+      bcc: string[],
+      from: string,
+      subject: string,
+      body: string | Array<{type: string, text: string}>
+    }>
+  }, cb: Function): Promise<any>;
+
+  /**
+   * Update A MailBots Task
+   * Used to save data against the task, update content, followup time and more
+   * @param {object}
+   * @param {object} params.task - Task object (see Create Task)
+   * @param {number} params.task.id - Task id being updated
+   * @param {string} [params.webhook] - Setting to true fires `task.updated` webhook
+   * @return {Promise}
+   *
+   * @example
+   *  const res = await mbClient.updateTask({
+    *    task: {
+    *     id: 1234,
+    *     reference_email: {
+    *       to: "newRecipient@exampletask.com"
+    *     }
+    *   }});
     */
-  sendEmail(email: Email, cb: Function): Promise<any>;
+   updateTask(params: {
+     task: {
+       id: number,
+       reference_email: {
+         to: string
+       }
+     }
+   }, cb: Function): Promise<any>;
+
+   /**
+   * Archive A MailBots Task
+   * @param {number} params - Arguments
+   * @param {number} params.task - Task object
+   * @param {number} params.task.id - Task id to complete
+   * @return {Promise}
+   *
+   * @example
+   */
+  completeTask(params: {
+    task: {
+      id: number
+    }
+  }, cb: Function): Promise<any>;
+
+  /**
+   * Permanently Delete A MailBots Task
+   * @param {object} params - Task to delete
+   * @param {object} params.task - Task object
+   * @param {number} params.task.id - Task id to delete
+   * @return {Promise}
+   */
+  deleteTask(params: {
+    task: {
+      id: number
+    }
+  }, cb: Function): Promise<void>;
+
+  /**
+   * Trigger a MailBots Task
+   * @param {object} params
+   * @param {boolean} params.trigger_url Trigger URL of the task to trigger (get from task object)
+   * @param {boolean} [params.verbose] Fire webhook and render HTML email response
+   * @return {Promise}
+   */
+  triggerTask(params: {
+    trigger_url: string,
+    payload: any,
+    verbose: boolean
+  }, cb: Function): Promise<any>;
+
+  /**
+   * Resolve Natural Time Format (ex: {naturaltime}@ext.eml.bot)
+   * @param {object} params - params object
+   * @param {string} params.format - Time format to check (ex: 3days)
+   * @param {string} params.timezone - IANA timezone designation (https://www.wikiwand.com/en/List_of_tz_database_time_zones) ex: "America/Los_Angeles"
+   * @return {Promise}
+   */
+  naturalTime(params: {
+    format: string,
+    timezone: string
+  }, cb: Function): Promise<any>;
+
+  /**
+   * Dispatch an email-based action for a task. (Equivalent to sending an action email.)
+   * @param {object} params
+   * @param {string} params.action - The action string
+   * @param {object} params.reference_email - The email that would be sent as the action email
+   * @param {boolean} [params.verbose] - Include rendered HTML email contents in API response
+   * @return {Promise}
+   */
+  sendAction(params: {
+    action: string,
+    reference_email: string,
+    verbose: boolean
+  }, cb: Function): Promise<any>;
 }
